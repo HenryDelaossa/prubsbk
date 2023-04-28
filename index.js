f24e5a6e645f90b38b23c61e12bffa82ce54b4db
HOSSA <hossa@gruponovus.com>
2023-04-27 19:08:52 -0500

    ip request4

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use("/", (req, res) => {
   );
 });
 
-router.use("/api", router);
 router.get("/ex", () => console.log("algo"));
+router.use("/api", router);
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("listen on port", port);
